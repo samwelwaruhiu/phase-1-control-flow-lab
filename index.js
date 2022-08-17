1096b74fd36f0b40905a69d7719c606be469b223
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-08-17 13:07:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if (ride <= 199){
+    return `This one is on me!`
+  }
+  else if (ride > 2000 && ride< 2500){
+    return`I will gladly take your thirty bucks.`
+  }
+  else
+    return `No can do.`
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === `NYC`? "Ok, sounds good." : "No go.";
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous" :
+      return "Thank you so much." 
+      break;
+
+      case "not as generous" :
+        return "Thank you."
+        break;
+
+        default:
+          return "Bye."
+          break;
+  }
   // Write your code here!
 }
